8402b354ce86882a840905e4f230c8467a77913a
Tomer-L <76324664+Tomer-L@users.noreply.github.com>
2024-11-05 11:46:55 +0200

    Fix proto files (#7)
    * fix: added missing files
    
    * fix: proto exports

--- code/flowlogs/src/models/protobuf/proto.js
+++ code/flowlogs/src/models/protobuf/proto.js
@@ -2,19 +2,25 @@ const protobuf = require('protobufjs')
 const protoFilePath = './src/models/protobuf/collection.proto'
 
 let pbRoot
+let flowLogsMsgProto
 let flowLogsBatchProto
 let FlowLogsDeviceTypeEnum
+let FlowLogActionEnum
 
 const loadProtobuf = () => {
   if (!pbRoot) {
     pbRoot = protobuf.loadSync(protoFilePath)
+    flowLogsMsgProto = pbRoot.lookupType('collection.VpcFlowLog')
     flowLogsBatchProto = pbRoot.lookupType('collection.VpcFlowLogs')
     FlowLogsDeviceTypeEnum = pbRoot.lookupEnum('collection.FlowLogsDeviceType')
+    FlowLogActionEnum = pbRoot.lookupEnum('collection.FlowLogAction')
   }
 
   return {
+    flowLogsMsgProto,
     flowLogsBatchProto,
     FlowLogsDeviceTypeEnum,
+    FlowLogActionEnum,
   }
 }
 
@@ -23,4 +29,6 @@ loadProtobuf()
 module.exports = {
   flowLogsBatchProto,
   FlowLogsDeviceTypeEnum,
+  FlowLogActionEnum,
+  flowLogsMsgProto,
 }
