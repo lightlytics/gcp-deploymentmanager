3d0d5422f088eee9994c3338b70e62babd2d969f
Tomer-L <76324664+Tomer-L@users.noreply.github.com>
2024-11-26 13:39:08 +0200

    reduce timeouts (#9)
    * fix: add filters for events sink
    
    * fix: reduce timeouts to 5 seconds
    
    * fix: error printing
    
    * reduced timeout for events cloud function

--- code/events/src/index.js
+++ code/events/src/index.js
@@ -10,6 +10,6 @@ functions.cloudEvent('streamsec-audit-logs-collector', async (cloudEvent) => {
     const response = await httpClient.postAuditEvent(data)
     console.log(`Sent log and got response ${response?.data} (${response?.status})`)
   } catch (error) {
-    console.error('Error sending log data:', error)
+    console.error(`Error sending log data (${error?.code || error?.response?.statusCode})`)
   }
 })
